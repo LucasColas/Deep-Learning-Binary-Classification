001bb690f8ff4fb33559d8ad91c39c623c0d5e0a
LucasColas <lucas.colas13@gmail.com>
2021-03-21 06:10:25 +0100

    update

--- main.py
+++ main.py
@@ -7,4 +7,10 @@ import matplotlib.pyplot as plt
 
 conv_base = VGG16(weights='imagenet', include_top=False, input_shape=(150,150,3))
 
-train_ = ImageDataGenerator(rescale=1./255)
+test = ImageDataGenerator(rescale=1./255) #Rescale is a value by which we will multiply the data
+#before any other processing. Our original images consist in RGB coefficients
+#in the 0-255, but such values would be too high for our model to process
+#(given a typical learning rate), so we target values between 0 and 1
+#instead by scaling with a 1/255
+train_datagen = ImageDataGenerator(rescale=1./255, rotation_range=40, width_shift_range=0.2, height_shift_range=0.2, shear_range=0.2, zoom_range=0.2, horizontal_flip = True)
+train_generator = train_datagen.
