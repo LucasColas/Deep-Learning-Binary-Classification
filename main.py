abebf2d6010951c7248b8529e6d8cf6e94666fc6
LucasColas <lucas.colas13@gmail.com>
2021-03-21 06:45:39 +0100

    update

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from tensorflow.keras import models
 from tensorflow.keras import layers
-from tensorflow.keras import metrics
+from tensorflow.keras import metrics, optimizers
 from tensorflow.keras.applications import VGG16
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 
 import matplotlib.pyplot as plt
@@ -19,7 +20,7 @@ test_datagen = ImageDataGenerator(rescale=1./255) #Rescale is a value by which w
 train_datagen = ImageDataGenerator(rescale=1./255, rotation_range=40, width_shift_range=0.2, height_shift_range=0.2, shear_range=0.2, zoom_range=0.2, horizontal_flip = True)
 
 train_generator = train_datagen.flow_from_directory(train_dir, target_size=(150,150), batch_size=32, class_mode='binary')
-validation_generator = test_datagen.flow_from_directory(validation_dir, target_size(150,150), batch_size=32, class_mode='binary')
+validation_generator = test_datagen.flow_from_directory(validation_dir, target_size=(150,150), batch_size=32, class_mode='binary')
 
 step_size_train = train_generator.n//train_generator.batch_size
 step_size_valid = validation_generator.n//validation_generator.batch_size
@@ -37,7 +38,7 @@ model.add(layers.Flatten())
 model.add(layers.Dense(512, activation='relu'))
 model.add(layers.Dense(1,activation='sigmoid'))
 
-
+model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4), metrics=['acc'])
 history = model.fit(train_generator, steps_per_epoch=step_size_train,epochs=20,validation_data=validation_generator, validation_steps=step_size_valid)
 
 acc = history.history['acc']
