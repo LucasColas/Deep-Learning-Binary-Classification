84bc863e5ba8e02c19009e127260da522775f25f
LucasColas <lucas.colas13@gmail.com>
2021-03-21 06:40:04 +0100

    update

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from keras.tensorflow import models
-from keras.tensorflow import layers
-from keras.tensorflow import metrics
-from keras.tensorflow.applications import VGG16
+from tensorflow.keras import models
+from tensorflow.keras import layers
+from tensorflow.keras import metrics
+from tensorflow.keras.applications import VGG16
+import numpy as np
 
 import matplotlib.pyplot as plt
 
@@ -24,6 +25,38 @@ step_size_train = train_generator.n//train_generator.batch_size
 step_size_valid = validation_generator.n//validation_generator.batch_size
 
 model = models.Sequential()
-model.add(layers.Conv2D(32,(3,3), activation='relu', input_shape(150,150,3)))
-model.add(layers.MaxPooling2D((2,2))
+model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(150,150,3)))
+model.add(layers.MaxPooling2D((2,2)))
 model.add(layers.Conv2D(64,(3,3), activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(128,(3,3), activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(128,(3,3), activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1,activation='sigmoid'))
+
+
+history = model.fit(train_generator, steps_per_epoch=step_size_train,epochs=20,validation_data=validation_generator, validation_steps=step_size_valid)
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1,len(acc)+1)
+
+plt.plot(epochs,acc,'bo', label='Training acc')
+plt.plot(epochs, val_acc,'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
