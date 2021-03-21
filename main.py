530507c03779f058eca97e440b75ef0c59b65cc9
LucasColas <lucas.colas13@gmail.com>
2021-03-21 06:01:39 +0100

    update

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from keras.tensorflow import layers
 from keras.tensorflow import metrics
 from keras.tensorflow.applications import VGG16
 
+import matplotlib.pyplot as plt
+
 conv_base = VGG16(weights='imagenet', include_top=False, input_shape=(150,150,3))
 
 train_ = ImageDataGenerator(rescale=1./255)
