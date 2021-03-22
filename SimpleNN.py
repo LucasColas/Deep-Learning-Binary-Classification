64388a04eee617c4142dc4cbb5a9a854ed57da4a
LucasColas <lucas.colas13@gmail.com>
2021-03-22 12:55:43 +0100

    update

--- SimpleNN.py
+++ SimpleNN.py
@@ -6,21 +6,15 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 
 from PIL import ImageFile
-ImageFile.LOAD_TRUNCATED_IMAGES = True #Fix an issue with PIL which reads truncated files
-
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 import matplotlib.pyplot as plt
 
 from create_DS import train_dir, validation_dir
 
-#conv_base = VGG16(weights='imagenet', include_top=False, input_shape=(150,150,3))
 
 
-test_datagen = ImageDataGenerator(rescale=1./255) #Rescale is a value by which we will multiply the data
-#before any other processing. Our original images consist in RGB coefficients
-#in the 0-255, but such values would be too high for our model to process
-#(given a typical learning rate), so we target values between 0 and 1
-#instead by scaling with a 1/255
+test_datagen = ImageDataGenerator(rescale=1./255) 
 train_datagen = ImageDataGenerator(rescale=1./255, rotation_range=40, width_shift_range=0.2, height_shift_range=0.2, shear_range=0.2, zoom_range=0.2, horizontal_flip = True)
 
 train_generator = train_datagen.flow_from_directory(train_dir, target_size=(150,150), batch_size=32, class_mode='binary', color_mode='rgba')
