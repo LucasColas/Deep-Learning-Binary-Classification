1e7d0457c49bb95f8fa5a46d83c4dc3bd99487c6
LucasColas <lucas.colas13@gmail.com>
2021-03-27 06:20:07 +0100

    update

--- NN_VGG16.py
+++ NN_VGG16.py
@@ -1,4 +1,5 @@
 from tensorflow.keras import VGG16
+import numpy as np
 
 
 conv_base = VGG16(weights="imagenet", include_top=False, input_shape=(150,150,4))
@@ -9,3 +10,10 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 import matplotlib.pyplot as plt
 
 from create_DS import train_dir, validation_dir
+
+
+model = models.Sequential()
+model.add(conv_base)
+model.add(layers.Flatten())
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
