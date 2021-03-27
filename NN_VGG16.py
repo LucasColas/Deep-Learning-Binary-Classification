a3e849e408be780cde8e4f83875252f8c663f4b0
LucasColas <lucas.colas13@gmail.com>
2021-03-27 06:22:21 +0100

    update

--- NN_VGG16.py
+++ NN_VGG16.py
@@ -1,4 +1,4 @@
-from tensorflow.keras import VGG16
+from tensorflow.keras.applications import VGG16
 import numpy as np
 
 
@@ -17,3 +17,12 @@ model.add(conv_base)
 model.add(layers.Flatten())
 model.add(layers.Dense(256, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
+
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+train_datagen = ImageDataGenerator(rescale=1./255, rotation_range=40, width_shift_range=0.2, height_shift_range=0.2, shear_range=0.2, zoom_range=0.2, horizontal_flip = True)
+
+train_generator = train_datagen.flow_from_directory(train_dir, target_size=(150,150), batch_size=32, class_mode='binary', color_mode='rgba')
+validation_generator = test_datagen.flow_from_directory(validation_dir, target_size=(150,150), batch_size=32, class_mode='binary', color_mode='rgba') #'rgba' because we have images with transparency
+
+print(train_generator[0].shape)
