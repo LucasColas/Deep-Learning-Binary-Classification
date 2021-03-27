b0a2c11f88297f3230be4f66e270b8fdc8f1388a
LucasColas <lucas.colas13@gmail.com>
2021-03-27 06:29:03 +0100

    update

--- NN_VGG16.py
+++ NN_VGG16.py
@@ -1,8 +1,11 @@
 from tensorflow.keras.applications import VGG16
+from tensorflow.keras import models, layers
 import numpy as np
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
-conv_base = VGG16(weights="imagenet", include_top=False, input_shape=(150,150,4))
+
+conv_base = VGG16(weights="imagenet", include_top=False, input_shape=(150,150,3))
 
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
@@ -25,4 +28,9 @@ train_datagen = ImageDataGenerator(rescale=1./255, rotation_range=40, width_shif
 train_generator = train_datagen.flow_from_directory(train_dir, target_size=(150,150), batch_size=32, class_mode='binary', color_mode='rgba')
 validation_generator = test_datagen.flow_from_directory(validation_dir, target_size=(150,150), batch_size=32, class_mode='binary', color_mode='rgba') #'rgba' because we have images with transparency
 
-print(train_generator[0].shape)
+for data_batch, labels_batch in train_generator:
+    print("shape of a data batch",data_batch.shape)
+
+    break
+
+    
