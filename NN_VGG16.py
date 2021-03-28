8e6753a89d29b2f9be4e1937098486bd917ed2d6
LucasColas <lucas.colas13@gmail.com>
2021-03-28 06:22:08 +0200

    update

--- NN_VGG16.py
+++ NN_VGG16.py
@@ -12,6 +12,8 @@ import matplotlib.pyplot as plt
 from create_DS import train_dir, validation_dir
 
 conv_base = VGG16(weights="imagenet", include_top=False, input_shape=(150,150,3))
+step_size_train = train_generator.n//train_generator.batch_size
+step_size_valid = validation_generator.n//validation_generator.batch_size
 
 
 model = models.Sequential()
@@ -36,3 +38,28 @@ for data_batch, labels_batch in train_generator:
     print("shape of a data batch",data_batch.shape)
 
     break
+
+
+history = model.fit(train_generator, steps_per_epoch=100, epochs=30,validation_data=validation_generator, validation_steps=50)
+model.save("NN VGG16.h5")
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1,len(acc)+1)
+
+plt.plot(epochs,acc,'bo', label='Training acc')
+plt.plot(epochs, val_acc,'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
