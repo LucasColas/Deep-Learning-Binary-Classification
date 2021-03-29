d8741c8bd2aea8bdf51af470f6c247119ef57f09
LucasColas <lucas.colas13@gmail.com>
2021-03-29 06:38:13 +0200

    update

--- NN_VGG16.py
+++ NN_VGG16.py
@@ -28,7 +28,7 @@ print("trainable weights : ", len(model.trainable_weights))
 
 
 test_datagen = ImageDataGenerator(rescale=1./255)
-train_datagen = ImageDataGenerator(rescale=1./255, rotation_range=40, width_shift_range=0.2, height_shift_range=0.2, shear_range=0.2, zoom_range=0.2, horizontal_flip = True)
+train_datagen = ImageDataGenerator(rescale=1./255, rotation_range=40, width_shift_range=0.2, height_shift_range=0.2, shear_range=0.2, zoom_range=0.2, horizontal_flip = True, fill_mode='nearest')
 
 train_generator = train_datagen.flow_from_directory(train_dir, target_size=(150,150), batch_size=32, class_mode='binary', color_mode='rgba')
 validation_generator = test_datagen.flow_from_directory(validation_dir, target_size=(150,150), batch_size=32, class_mode='binary', color_mode='rgba') #'rgba' because we have images with transparency
