54e526317dcaa32c3a4b934a0664469365a2607e
LucasColas <lucas.colas13@gmail.com>
2021-03-31 17:04:47 +0200

    update

--- NN_VGG16.py
+++ NN_VGG16.py
@@ -89,14 +89,14 @@ epochs = range(1,len(acc)+1)
 
 plt.plot(epochs,acc,'bo', label='Training acc')
 #plt.plot(epochs, val_acc,'b', label='Validation acc')
-plt.title('Training and validation accuracy')
+plt.title('Training accuracy')
 plt.legend()
 
 plt.figure()
 
 plt.plot(epochs, loss, 'bo', label='Training loss')
 #plt.plot(epochs, val_loss, 'b', label='Validation loss')
-plt.title('Training and validation loss')
+plt.title('Training loss')
 plt.legend()
 
 plt.show()
