8a648c735deae206d8a7f8711a384935ea6ae653
LucasColas <lucas.colas13@gmail.com>
2021-04-02 06:26:19 +0200

    update

--- VVGG16_test.py
+++ VVGG16_test.py
@@ -4,19 +4,23 @@ import os
 import matplotlib.pyplot as plt
 
 from create_DS import test_dir_glasses
-
-
+path_img = os.listdir(test_dir_glasses)
+image = []
 
 for img in test_dir_glasses:
     try:
-        img_path = os.path.join(test_dir_glasses, "glass_2001.jpg")
+        img_path = os.path.join(test_dir_glasses, img)
         print(img_path)
         img = cv2.imread(os.path.join(test_dir_glasses, "glass_2001.jpg"))
         new_img = cv2.resize(img, (150,150))
+        image.append(new_img)
     except Exception as e:
         print(str(e))
 
-    break
+    if len(image) >= 1:
+        break
+
+
 
 plt.imshow(new_img)
 plt.show()
