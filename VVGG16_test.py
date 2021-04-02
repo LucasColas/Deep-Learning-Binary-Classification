2b8e7f9ea4653feddde71a402dddd70d373092f2
LucasColas <lucas.colas13@gmail.com>
2021-04-02 06:13:08 +0200

    update

--- VVGG16_test.py
+++ VVGG16_test.py
@@ -1,10 +1,21 @@
 from tensorflow.keras import models
+import cv2
+import os
+import matplotlib.pyplot as plt
 
 from create_DS import test_dir_glasses
 
 
+
+img = cv2.imread(os.path.join(test_dir_glasses, "glass_2001.jpg"))
+new_img = cv2.resize(img, (150,150))
+plt.imshow(new_img)
+plt.show()
+
 model = models.load_model("NN VGG16.h5")
 model.summary()
+
+
 
 
 
