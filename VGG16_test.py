2076f6950582f3b5220bedcc993df011c8e38103
LucasColas <lucas.colas13@gmail.com>
2021-04-03 06:29:57 +0200

    update

--- VGG16_test.py
+++ VGG16_test.py
@@ -6,13 +6,11 @@ import numpy as np
 
 from create_DS import test_dir_glasses,train_dir, validation_dir, test_dir
 
-image = []
 
 
 path = os.path.join(test_dir, "Glasses")
 print(path)
 path_img = os.listdir(path)
-print(path_img)
 images = []
 count = 0
 for img in path_img:
@@ -25,34 +23,39 @@ for img in path_img:
         break
     count += 1
 
-X = np.array(images, dtype='float').reshape(-1, 150,150,3)
-X /= 255
+path = os.path.join(test_dir, "Tables")
+print(path)
+path_img = os.listdir(path)
 
-
-"""
+count = 0
 for img in path_img:
-    try:
-        img_path = os.path.join(test_dir_glasses, img)
-        print(img_path)
-        img = cv2.imread(os.path.join(test_dir_glasses, "glass_2001.jpg"))
-        new_img = cv2.resize(img, (150,150))
-        image.append(new_img)
-    except Exception as e:
-        print(str(e))
-
-    if len(image) >= 1:
+    img_array = cv2.imread(os.path.join(path,img))
+    new_img_array = cv2.resize(img_array,(150,150))
+    images.append(new_img_array)
+    #plt.imshow(new_img_array)
+    #plt.show()
+    if count > 12:
         break
-
-"""
-
-
+    count += 1
 
 model = models.load_model("NN VGG16.h5")
 model.summary()
 
-def prediction(X):
-    pass
+def prediction(images):
+    n_glasses = 0
+    n_tables = 0
+    for image in images:
+        X = np.array(image, dtype='float').reshape(-1, 150,150,3)
+        X /= 255
+        predict = model.predict(X)
+        if predict <= 0.5:
+            print("glass")
+            n_glasses += 1
 
-print(X.shape)
-predict = model.predict(X)
-print(predict)
+        else:
+            print("table")
+            n_tables += 1
+
+    print(n_glasses, n_tables)
+
+prediction(images)
